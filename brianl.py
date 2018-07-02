1d863fcfc9d4899a356aafa8675d27451fb1e61f
EC2 Default User <ec2-user@ip-172-31-31-13.us-west-2.compute.internal>
2018-07-02 08:08:54 +0000

    enable debug mode

--- brianl.py
+++ brianl.py
@@ -6,4 +6,4 @@ def hello():
     return "<h1 style='color:blue'>Hello There!</h1>"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080, debug=True)
